deca5b55a9da792dda3e2f2a6917042735c7e423
kasarednyaneshwar <107169022+kasarednyaneshwar@users.noreply.github.com>
2022-11-03 14:49:21 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+class Multiplication
+{
+   static void add(int n)
+{
+  for (int i;i<=n;i++)
+   {
+     System.out.println(n ,i,n*i);
+   }
+}
+public static void main(String args[])
+{
+ multiplication(n:4);
+}
+
+}
